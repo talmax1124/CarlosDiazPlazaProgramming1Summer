209169f874cdd85e0bdb1b6fe419df6a6abbecb8
Carlos <carlitosdiazplaza@gmail.com>
2024-07-29 11:54:31 -0400

    Update main.c

--- Programming1Summer/main.c
+++ Programming1Summer/main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 // min + rand() % (max - min + 1);
-// *t++ char *token = strtok(line, ","); fgets(destination, size, pointer source)
+// *t++ char *token = strtok(line, ","); fgets(destination, size, pointer source) fgets(temp, size, fileptr) != NULL && x < ROWS
 
 int main(void){
 
