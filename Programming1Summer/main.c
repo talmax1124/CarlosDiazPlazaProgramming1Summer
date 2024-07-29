0d9cb3b276946abcfc3a2e1923ac6f15872bab86
talmax1124 <carlitosdiazplaza@gmail.com>
2024-07-29 02:11:42 -0400

    main.c

---
+++ Programming1Summer/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// min + rand() % (max - min + 1);
+// *t++ char *token = strtok(line, ","); fgets(destination, size, pointer source)
+
+int main(void){
+
+
+
+    return 0;
+}
